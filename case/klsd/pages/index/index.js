81b72f31a5b6c809774b09b5b1db5246bc91f66f
Jane <994824764@qq.com>
2023-07-05 16:28:15 +0800

    案例 - 克鲁赛德战记 - 详情页

--- case/klsd/pages/index/index.js
+++ case/klsd/pages/index/index.js
@@ -3,5 +3,12 @@ import heroList from '../../data/heroList'
 Page({
   data:{
     heroList: heroList
+  },
+  toDetail(e){
+    let {heroname, icon, skill} = e.currentTarget.dataset
+    // 跳转到详情页面
+    wx.navigateTo({
+      url: `/pages/detail/index?heroname=${heroname}&icon=${icon}&skill=${skill}`
+    })
   }
 })
