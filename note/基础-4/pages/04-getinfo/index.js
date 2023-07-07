c359246132e0728779751abbe65b1a64e2cc7c67
Jane <994824764@qq.com>
2023-07-07 18:24:14 +0800

    内置API - 上传头像

--- note/基础-4/pages/04-getinfo/index.js
+++ note/基础-4/pages/04-getinfo/index.js
@@ -10,5 +10,16 @@ Page({
     this.setData({
       'profile.avatarUrl':ev.detail.avatarUrl
     })
+    // 如果是从相册中选择的话，我们一种会将这个图片上传到服务器
+    // 接收返回的图片地址，再将图片渲染到页面上
+    // 上传临时文件
+    wx.uploadFile({
+      url: 'http://ajax-api.itheima.net/api/file',
+      filePath: ev.detail.avatarUrl,
+      name: 'avatar',
+      success: (res) => {
+        console.log(res);
+      },
+    });
   }
 })
